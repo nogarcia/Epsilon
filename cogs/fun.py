967567fcc9ae8120bc4fca45f1a418b804e89bed
Shrubhog <noah@danielgarcia.info>
2019-11-05 12:21:41 -0600

    kaomoji and config example

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,6 +2,7 @@
 Fun cog for Epsilon. Holds all fun commands.
 """
 
+import json
 from random import randint
 import discord
 from discord.ext import commands
@@ -12,8 +13,9 @@ class FunCog(commands.Cog):
     """
     Fun cog.
     """
-    def __init__(self, bot):
+    def __init__(self, bot, config):
         self.bot = bot
+        self.config = config
 
     def get_xkcd(self, comic_id=0):
         """
@@ -50,5 +52,22 @@ class FunCog(commands.Cog):
         embed.add_field(name="Text", value=comic_text, inline=True)
         await ctx.send(embed=embed)
 
+    @commands.command()
+    async def kaomoji(self, ctx, *, kaomote: str = None):
+        """
+        Print a kaomoji.
+        """
+        if kaomote is None or len(kaomote) == 0:
+            return
+        # Ensure markdown characters are escaped. "
+        # "._____." will show up as "._." with italics without escape characters.
+        config_kaomote = self.config["kaomoji"]["kaomotes"][kaomote]
+        if config_kaomote is None:
+            return
+        await ctx.send(config_kaomote)
+
+
 def setup(bot):
-    bot.add_cog(FunCog(bot))
+    with open('config/config.json', 'r') as f:
+        CONFIG = json.load(f)
+    bot.add_cog(FunCog(bot, CONFIG))
